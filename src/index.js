11e02b81929697f1bda1815bd379851e9ffa3d82
Chidi <ikebujochidi@gmail.com>
2025-06-05 13:24:11 -0400

    Added typewrite effect

--- src/index.js
+++ src/index.js
@@ -1,11 +1,3 @@
-//Typewriter for Hi There!
-// new Typewriter("#typewriter", {
-//   strings: "Hi There!",
-//   autoStart: true,
-//   cursor: "",
-//   delay: 100,
-// });
-
 // Get references to elements
 const hamburgerMenu = document.getElementById("hamburger-menu");
 const mobileMenu = document.getElementById("mobile-menu");
@@ -21,4 +13,37 @@ menuItems.forEach((item) => {
   item.addEventListener("click", () => {
     mobileMenu.classList.remove("active"); // Remove 'active' class to hide menu
   });
+});
+
+// typewriter effect
+
+window.addEventListener("DOMContentLoaded", () => {
+  // If prefers-reduced-motion, just write a static title:
+  const prefersReduced = window.matchMedia(
+    "(prefers-reduced-motion: reduce)"
+  ).matches;
+
+  const target = document.getElementById("role-typewriter");
+
+  if (prefersReduced) {
+    target.textContent = "Product Management";
+    return;
+  }
+
+  const typewriter = new Typewriter(target, {
+    loop: true,
+    delay: 80, // faster typing per char
+    deleteSpeed: 50, // faster deletion per char
+    cursor: "", // hide blinking cursor
+    autoStart: true,
+  });
+
+  typewriter
+    .typeString("Product Mgt & Development")
+    .pauseFor(1500)
+    .deleteAll()
+    .typeString("AI/ML & Data Science")
+    .pauseFor(1500)
+    .deleteAll()
+    .start();
 });
